cf17c1613234fdca937c8ea45f5e6e7848a9fc8b
QuantumGorilla <manotasja@uninorte.edu.co>
2020-12-19 13:07:35 -0500

    Heroku deployment

--- BoturuBot.py
+++ BoturuBot.py
@@ -1,30 +1,42 @@
+import logging, os, telegram
 from telegram.ext import Updater, CommandHandler
-import nekos, requests, re, random
+import nekos, requests, re, random, sys
 
+#Config logging
+
+logging.basicConfig(
+    level = logging.INFO, format="%(asctime) - %(name)s - %(levelname)s - %(message)s,"
+)
+
+logger = logging.getLogger()
+
+#Ask Token
+TOKEN = os.getenv("TOKEN")
+mode = os.getevn("MODE")
+
+if mode == "dev":
+    def run(updater):
+        updater.start_polling()
+        updater.idle()
+elif mode == "prod":
+    def run(updater):
+        PORT = int(os.environ.get("PORT","8443"))
+        HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
+        updater.star_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
+        updater.bot.set_webhook(f"https://{HEROKU_APP_NAME}.herokuapp.com/{TOKEN}")
+else:
+    logger.info('Mode not specified.')
+    sys.exit()
 
 def help(update, context):
     update.message.reply_text("""
-                              Senpai, esto son los comandos u//u: 
+                              Senpai, estos son los comandos u//u: 
                               \n /help - Mostrar los comandos
                               \n /doggo - Foto random de un doggo
                               \n /neko - Foto random de un neko
                               \n /hentai - NFSW ;)
                               """)
 
-def random_target():
-    possible = ['feet', 'yuri', 'trap', 'futanari', 'hololewd', 'lewdkemo',
-                'cum', 'erokemo', 'les', 'wallpaper', 'lewdk', 'ngif', 'lewd', 'feed',
-                'gecg', 'eroyuri', 'eron', 'cum_jpg', 'nsfw_neko_gif', 'solo', 'kemonomimi',
-                'nsfw_avatar', 'anal', 'erofeet', 'blowjob', 'pussy', 'tits', 'holoero', 'pussy_jpg',
-                'waifu', 'kiss', 'femdom', 'neko', 'spank', 'cuddle', 'erok', 'fox_girl', 'boobs',
-                'random_hentai_gif', 'smallboobs', 'ero']
-    return random.choice(possible)
-
-
-def hentai(update, context):
-    context.bot.send_photo(
-        chat_id=update.effective_chat.id, photo=nekos.img(random_target()))
-    
 def get_url(identifier):
     if identifier == 0:
         contents = requests.get('https://random.dog/woof.json').json()
@@ -46,7 +58,6 @@ def get_image_url(identifier):
             file_extension = re.search("([^.]*)$", url).group(0).lower()
     return url
 
-
 def doggo(update, context):
     url = get_image_url(0)
     context.bot.send_photo(chat_id=update.effective_chat.id, photo=url)
@@ -55,17 +66,28 @@ def  neko(update, context):
     url = get_image_url(1)
     context.bot.send_photo(chat_id=update.effective_chat.id, photo=url)
 
+def random_target():
+    possible = ['feet', 'yuri', 'trap', 'futanari', 'hololewd', 'lewdkemo',
+                'cum', 'erokemo', 'les', 'wallpaper', 'lewdk', 'ngif', 'lewd', 'feed',
+                'gecg', 'eroyuri', 'eron', 'cum_jpg', 'nsfw_neko_gif', 'solo', 'kemonomimi',
+                'nsfw_avatar', 'anal', 'erofeet', 'blowjob', 'pussy', 'tits', 'holoero', 'pussy_jpg',
+                'waifu', 'kiss', 'femdom', 'neko', 'spank', 'cuddle', 'erok', 'fox_girl', 'boobs',
+                'random_hentai_gif', 'smallboobs', 'ero']
+    return random.choice(possible)
+
+
+def hentai(update, context):
+    context.bot.send_photo(
+        chat_id=update.effective_chat.id, photo=nekos.img(random_target()))
+    
 def main():
-    updater = Updater(
-        token='1443354425:AAF5mOMsV6lGXJXDGMv7bDAM6-YYeJp_UnA', use_context=True)
+    Boturu = telegram.Bot(token = TOKEN)
+    updater = Updater(Boturu.token, use_context=True)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('help', help))
     dp.add_handler(CommandHandler('doggo', doggo))
     dp.add_handler(CommandHandler('neko', neko))
     dp.add_handler(CommandHandler('hentai', hentai))
-    updater.start_polling()
-    updater.idle()
-
 
 if __name__ == '__main__':
     main()
