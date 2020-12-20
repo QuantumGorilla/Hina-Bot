3f699838340c76df11113e2ee30e02a82deffefd
QuantumGorilla <manotasja@uninorte.edu.co>
2020-12-20 18:44:10 -0500

    Minor fix

--- BoturuBot.py
+++ BoturuBot.py
@@ -130,8 +130,7 @@ def main():
     dp.add_handler(CommandHandler('hey', hey))
     dp.add_handler(CommandHandler('navidad', navidad))
     dp.add_handler(CommandHandler('quien', quien))
-    updater.start_polling()
-    updater.idle()
+    updater.run()
     
 if __name__ == '__main__':
     main()
