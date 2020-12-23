281dcbe9ec360a10b26ce9cd1dec5369ab7d7ec1
QuantumGorilla <manotasja@uninorte.edu.co>
2020-12-22 23:50:37 -0500

    New command: despegala and improvements on code

--- tests/Hina_tests.py
+++ tests/Hina_tests.py
@@ -1,21 +1,6 @@
 from telegram.ext import Updater, CommandHandler
 import nekos, requests, re, random, os
 
-def help(update, context):
-    update.message.reply_text("""
-                              Senpai, estos son los comandos u//u: 
-                              \n /help - Mostrar los comandos
-                              \n /doggo - Foto random de un doggo
-                              \n /neko - Foto random de un neko
-                              \n /hentai - NFSW ;)
-                              \n /oilo - Oilo
-                              \n /pat - uwu
-                              \n /baka - Baka >_<
-                              \n /hey - Hey, loco, que pasa valemia
-                              \n /navidad - la navidad es todo aquello
-                              \n /quien - Quién monda es Dorian?  
-                              """)
-
 def get_url(identifier):
     if identifier == 0:
         contents = requests.get('https://random.dog/woof.json').json()
@@ -70,7 +55,6 @@ def pat(update, context):
         context.bot.send_photo(chat_id=update.effective_chat.id, photo=nekos.img('pat'))
 
 def baka(update, context):
-    user = update.message.from_user
     if len(context.args) != 0:
         update.message.reply_text('B-b-baka, '+ context.args[0] + ' ! >_<')
         context.bot.send_photo(chat_id=update.effective_chat.id, photo=nekos.img('baka'))
@@ -78,14 +62,14 @@ def baka(update, context):
         context.bot.send_photo(chat_id=update.effective_chat.id, photo=nekos.img('baka'))
     
 def hey(update, context):
-    context.bot.send_video(chat_id=update.message.chat_id, video=open('media/Hey.mp4', 'rb'), supports_streaming=True)
+    context.bot.send_video(chat_id=update.effective_chat.id, video=open('media/Hey.mp4', 'rb'), supports_streaming=True)
 
 def navidad(update, context):
     update.message.reply_text('Feliz navidad, s-senpai! uwu')
-    context.bot.send_audio(chat_id=update.message.chat_id, audio=open('media/Navidad.mp3', 'rb'))
+    context.bot.send_audio(chat_id=update.effective_chat.id, audio=open('media/Navidad.mp3', 'rb'))
     
 def quien(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text='Quién?')
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Quién?')
     context.bot.send_audio(chat_id=update.message.chat_id, audio=open('media/Quien.mp3', 'rb'))
 
 def buenosdias(update,context):
@@ -96,21 +80,23 @@ def buenasnoches(update, context):
     file = random.choice(os.listdir('media/buenas_noches'))
     context.bot.send_photo(chat_id=update.effective_chat.id, photo=open('media/buenas_noches/'+file, 'rb'))
     
+def princesas(update, context):
+    context.bot.send_video(chat_id=update.effective_chat.id, video=open('media/Princesas.mp4', 'rb'), supports_streaming=True)
+
+def despegala(update, context):
+    try:    
+        user = update.message.reply_to_message
+        context.bot.send_message(chat_id=update.effective_chat.id, text='Omae wa mou... Shindeiru')
+        context.bot.kickChatMember(chat_id=update.effective_chat.id, user_id=user.from_user['id'])
+    except:
+        context.bot.send_message(chat_id=update.effective_chat.id, text='Hpta, el cv es admin')
+    
 def main():
     updater = Updater(token='TOKEN', use_context=True)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('help', help))
-    dp.add_handler(CommandHandler('doggo', doggo))
-    dp.add_handler(CommandHandler('neko', neko))
-    dp.add_handler(CommandHandler('hentai', hentai))
-    dp.add_handler(CommandHandler('oilo', oilo))
-    dp.add_handler(CommandHandler('pat', pat))
+    dp.add_handler(CommandHandler('despegala', despegala))
     dp.add_handler(CommandHandler('baka', baka))
-    dp.add_handler(CommandHandler('hey', hey))
-    dp.add_handler(CommandHandler('navidad', navidad))
-    dp.add_handler(CommandHandler('quien', quien))
-    dp.add_handler(CommandHandler('buenosdias', buenosdias))
-    dp.add_handler(CommandHandler('buenasnoches', buenasnoches))
     updater.start_polling()
     updater.idle()
     
