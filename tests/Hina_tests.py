6c919b5ac69dc16e8fd0e98b78a433825434e1a7
QuantumGorilla <manotasja@uninorte.edu.co>
2021-01-15 03:30:40 -0500

    Some categories removed from random_target

--- tests/Hina_tests.py
+++ tests/Hina_tests.py
@@ -1,46 +1,24 @@
+import telegram
 from telegram.ext import Updater, CommandHandler
-import nekos
+import nekos, random
 
-def pat(update, context):
-    reply_user = update.message.reply_to_message
-    user = update.message.from_user
-    if reply_user == None:
-        if len(context.args) != 0:
-            word = ' '
-            context.bot.send_message(chat_id=update.effective_chat.id, text='@' + user['username'] + ' is patting ' + word.join(context.args) + ' uwu')
-            context.bot.send_photo(chat_id=update.effective_chat.id, photo=nekos.img('pat'))
-        else:
-            context.bot.send_photo(chat_id=update.effective_chat.id, photo=nekos.img('pat'))
-    else:
-        if reply_user.from_user['username'] == None:
-            context.bot.send_message(chat_id=update.effective_chat.id, text='@' + user['username'] + ' is patting ' + reply_user.from_user['first_name'] + ' uwu')
-            context.bot.send_photo(chat_id=update.effective_chat.id, photo=nekos.img('pat'))
-        else:
-            context.bot.send_message(chat_id=update.effective_chat.id, text='@' + user['username'] + ' is patting ' + reply_user.from_user['first_name'] + ' ' + '(@'+ reply_user.from_user['username'] + ')' + ' uwu')
-            context.bot.send_photo(chat_id=update.effective_chat.id, photo=nekos.img('pat'))
 
-def baka(update, context):
-    reply_user = update.message.reply_to_message
-    if reply_user == None:        
-        if len(context.args) != 0:
-            word = ' '
-            context.bot.send_message(chat_id=update.effective_chat.id, text='B-b-baka, ' + word.join(context.args) + ' ! >_<')
-            context.bot.send_photo(chat_id=update.effective_chat.id, photo=nekos.img('baka'))
-        else:   
-            context.bot.send_photo(chat_id=update.effective_chat.id, photo=nekos.img('baka'))
-    else:
-        if reply_user.from_user['username'] == None:
-            context.bot.send_message(chat_id=update.effective_chat.id, text='B-b-baka, ' + reply_user.from_user['first_name'] + ' ! >_<')
-            context.bot.send_photo(chat_id=update.effective_chat.id, photo=nekos.img('baka'))
-        else:
-            context.bot.send_message(chat_id=update.effective_chat.id, text='B-b-baka, ' + reply_user.from_user['first_name'] + '(@' + reply_user.from_user['username'] + ')'+ ' ! >_<')
-            context.bot.send_photo(chat_id=update.effective_chat.id, photo=nekos.img('baka'))    
+def random_target():
+    possible = ['feet', 'yuri', 'trap', 'futanari', 'hololewd', 'lewdkemo',
+                'cum', 'erokemo', 'les', 'lewdk', 'lewd', 'eroyuri', 'eron', 
+                'cum_jpg', 'nsfw_neko_gif', 'solo', 'anal', 'blowjob', 'pussy', 
+                'tits', 'holoero', 'pussy_jpg', 'femdom', 'spank', 'erok', 'boobs',
+                'ero']
+    return random.choice(possible)
 
+def hentai(update, context):
+    context.bot.send_photo(
+        chat_id=update.effective_chat.id, photo=nekos.img(random_target()))
+    
 def main():
-    updater = Updater(token='TOKEN', use_context=True)
+    updater = Updater(token='1448142051:AAFGjpOMeUn8Cat2pDIMKUAragLVgFkXaMQ', use_context=True)
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler('pat', pat))
-    dp.add_handler(CommandHandler('baka', baka))
+    dp.add_handler(CommandHandler('hentai', hentai))
     updater.start_polling()
     updater.idle()
     
