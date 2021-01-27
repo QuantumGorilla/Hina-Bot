07a4aa8aa5853a96d475dba6cb3831ea1d03ae62
QuantumGorilla <manotasja@uninorte.edu.co>
2021-01-27 08:26:19 -0500

    New command arrives: abueno

--- BoturuBot.py
+++ BoturuBot.py
@@ -30,7 +30,7 @@ else:
 
 def taskete(update, context):
      update.message.reply_text("""
-                              Senpai, estos son los comandos u//u: \n /taskete - Mostrar los comandos \n /doggo - Foto random de un doggo \n /neko - Foto random de un neko \n /hentai - NSFW ;) \n /dollar - Every dollar spent on... \n /oilo - Oilo \n /pat - uwu \n /baka - Baka >_< \n /hey - Hey, loco, que pasa valemia \n /navidad - la navidad es todo aquello \n /quien - Quién monda es Dorian? \n /buenosdias - Ohayo, darin! (Chayanne) \n /buenasnoches - Piolin te desea buenas noches \n /princesas - Comando especial para Valeria \n /noticias - Noticias iconicas de Colombia \n /despegala - Despegala, cachón \n /metienesque - Me tienes que sopletear \n /comedia - Donco media \n /mimir - Hora de mimir \n /die - I just wanna die \n /uypah - Uy, pah, lo dijiteeeeeeeee \n /respete - No, señor, respete \n /escribebien - Escribe bien, cachón \n /no - No \n /si - Sí \n /simp - SIMP \n /ayno - Ay, no, eso sí jamás \n /bye - La despego \n /perro - Perro con perro \n /cagaste - Cagaste, master \n /fino - Fino, mi rey \n /re - REEEEEEEEE
+                              Senpai, estos son los comandos u//u: \n /taskete - Mostrar los comandos \n /doggo - Foto random de un doggo \n /neko - Foto random de un neko \n /hentai - NSFW ;) \n /dollar - Every dollar spent on... \n /oilo - Oilo \n /pat - uwu \n /baka - Baka >_< \n /hey - Hey, loco, que pasa valemia \n /navidad - la navidad es todo aquello \n /quien - Quién monda es Dorian? \n /buenosdias - Ohayo, darin! (Chayanne) \n /buenasnoches - Piolin te desea buenas noches \n /princesas - Comando especial para Valeria \n /noticias - Noticias iconicas de Colombia \n /despegala - Despegala, cachón \n /metienesque - Me tienes que sopletear \n /comedia - Donco media \n /mimir - Hora de mimir \n /die - I just wanna die \n /uypah - Uy, pah, lo dijiteeeeeeeee \n /respete - No, señor, respete \n /escribebien - Escribe bien, cachón \n /no - No \n /si - Sí \n /simp - SIMP \n /ayno - Ay, no, eso sí jamás \n /bye - La despego \n /perro - Perro con perro \n /cagaste - Cagaste, master \n /fino - Fino, mi rey \n /re - REEEEEEEEE \n /abueno - Te me cuidas, crack
                               """)
 
 def get_url(identifier):
@@ -311,6 +311,24 @@ def fino(update, context):
 def re(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text='REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE')
     context.bot.send_audio(chat_id=update.effective_chat.id, audio=open('./media/Re.mp3', 'rb'))
+
+def abueno(update, context):
+    reply_user = update.message.reply_to_message
+    file = random.choice(os.listdir('media/abueno'))
+    if reply_user == None:
+        if len(context.args) != 0:
+            word = ' '
+            context.bot.send_message(chat_id=update.effective_chat.id, text='Te me cuidas, crack (' + word.join(context.args) + ')')
+            context.bot.send_photo(chat_id=update.effective_chat.id, photo=open('./media/abueno/'+file, 'rb'))
+        else:
+            context.bot.send_photo(chat_id=update.effective_chat.id, photo=open('./media/abueno/'+file, 'rb'))
+    else:
+        if reply_user.from_user['username'] == None:
+            context.bot.send_message(chat_id=update.effective_chat.id, text='Te me cuidas, crack (' + reply_user.from_user['first_name'] + ')')
+            context.bot.send_photo(chat_id=update.effective_chat.id, photo=open('./media/abueno/'+file, 'rb'))
+        else:
+            context.bot.send_message(chat_id=update.effective_chat.id, text='Te me cuidas, crack (@'+ reply_user.from_user['username'] + ')')
+            context.bot.send_photo(chat_id=update.effective_chat.id, photo=open('./media/abueno/'+file, 'rb'))
     
 def main():
     Boturu = telegram.Bot(token = TOKEN)
@@ -348,6 +366,7 @@ def main():
     dp.add_handler(CommandHandler('cagaste', cagaste))
     dp.add_handler(CommandHandler('fino', fino))
     dp.add_handler(CommandHandler('re', re))
+    dp.add_handler(CommandHandler('abueno', abueno))
     run(updater)
     
 if __name__ == '__main__':
