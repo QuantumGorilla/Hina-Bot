fca3df7ef67caceffeef7e3a066ef6cd22ead3e9
QuantumGorilla <manotasja@uninorte.edu.co>
2021-03-07 11:14:53 -0500

    New command arrives: hya and video added to sapo command

--- BoturuBot.py
+++ BoturuBot.py
@@ -30,7 +30,7 @@ else:
 
 def taskete(update, context):
      update.message.reply_text("""
-                              Senpai, estos son los comandos u//u: \n /taskete - Mostrar los comandos \n /doggo - Foto random de un doggo \n /neko - Foto random de un neko \n /hentai - NSFW ;) \n /dollar - Every dollar spent on... \n /oilo - Oilo \n /pat - uwu \n /baka - Baka >_< \n /hey - Hey, loco, que pasa valemia \n /navidad - la navidad es todo aquello \n /quien - Quién monda es Dorian? \n /buenosdias - Ohayo, darin! (Chayanne) \n /buenasnoches - Piolin te desea buenas noches \n /princesas - Comando especial para Valeria \n /noticias - Noticias iconicas de Colombia \n /despegala - Despegala, cachón \n /metienesque - Me tienes que sopletear \n /comedia - Donco media \n /mimir - Hora de mimir \n /die - I just wanna die \n /uypah - Uy, pah, lo dijiteeeeeeeee \n /respete - No, señor, respete \n /escribebien - Escribe bien, cachón \n /no - No \n /si - Sí \n /simp - SIMP \n /ayno - Ay, no, eso sí jamás \n /bye - La despego \n /perro - Perro con perro \n /cagaste - Cagaste, master \n /fino - Fino, mi rey \n /re - REEEEEEEEE \n /abueno - Te me cuidas, crack \n /callese - Me tiene jodidamente mamado \n /horny - Estoy horny \n /meto - Hijuputa, tetra hijueputa \n /rico - Rico hpta \n /kya - Kya~ \n /dato - Que buen dato, crack \n /antojaron - Ya antojaron \n /chirrete - Me viste cara de chirrete? \n /decepcion - Que decepción \n /tragatela - Bueno, tragatela \n /sapo - Cule sapo
+                              Senpai, estos son los comandos u//u: \n /taskete - Mostrar los comandos \n /doggo - Foto random de un doggo \n /neko - Foto random de un neko \n /hentai - NSFW ;) \n /dollar - Every dollar spent on... \n /oilo - Oilo \n /pat - uwu \n /baka - Baka >_< \n /hey - Hey, loco, que pasa valemia \n /navidad - la navidad es todo aquello \n /quien - Quién monda es Dorian? \n /buenosdias - Ohayo, darin! (Chayanne) \n /buenasnoches - Piolin te desea buenas noches \n /princesas - Comando especial para Valeria \n /noticias - Noticias iconicas de Colombia \n /despegala - Despegala, cachón \n /metienesque - Me tienes que sopletear \n /comedia - Donco media \n /mimir - Hora de mimir \n /die - I just wanna die \n /uypah - Uy, pah, lo dijiteeeeeeeee \n /respete - No, señor, respete \n /escribebien - Escribe bien, cachón \n /no - No \n /si - Sí \n /simp - SIMP \n /ayno - Ay, no, eso sí jamás \n /bye - La despego \n /perro - Perro con perro \n /cagaste - Cagaste, master \n /fino - Fino, mi rey \n /re - REEEEEEEEE \n /abueno - Te me cuidas, crack \n /callese - Me tiene jodidamente mamado \n /horny - Estoy horny \n /meto - Hijuputa, tetra hijueputa \n /rico - Rico hpta \n /kya - Kya~ \n /dato - Que buen dato, crack \n /antojaron - Ya antojaron \n /chirrete - Me viste cara de chirrete? \n /decepcion - Que decepción \n /tragatela - Bueno, tragatela \n /sapo - Cule sapo \n /hya - #HYAPOSTING
                               """)
 
 def get_url(identifier):
@@ -427,20 +427,25 @@ def tragatela(update, context):
 
 def sapo(update, context):
     reply_user = update.message.reply_to_message
+    file = random.choice(os.listdir('media/sapo'))
     if reply_user == None:
         if len(context.args) != 0:
             word = ' '
             context.bot.send_message(chat_id=update.effective_chat.id, text='Cule sapo, ' + word.join(context.args))
-            context.bot.send_video(chat_id=update.effective_chat.id, video=open('./media/Sapo.mp4', 'rb'), supports_streaming=True)
+            context.bot.send_video(chat_id=update.effective_chat.id, video=open('./media/sapo/'+file, 'rb'), supports_streaming=True)
         else:
-            context.bot.send_video(chat_id=update.effective_chat.id, video=open('./media/Sapo.mp4', 'rb'), supports_streaming=True)
+            context.bot.send_video(chat_id=update.effective_chat.id, video=open('./media/sapo/'+file, 'rb'), supports_streaming=True)
     else:
         if reply_user.from_user['username'] == None:
             context.bot.send_message(chat_id=update.effective_chat.id, text='Cule sapo, ' + reply_user.from_user['first_name'])
-            context.bot.send_video(chat_id=update.effective_chat.id, video=open('./media/Sapo.mp4', 'rb'), supports_streaming=True)
+            context.bot.send_video(chat_id=update.effective_chat.id, video=open('./media/sapo/'+file, 'rb'), supports_streaming=True)
         else:
             context.bot.send_message(chat_id=update.effective_chat.id, text='Cule sapo, '+ reply_user.from_user['first_name'] + ' (@' + reply_user.from_user['username'] + ')')
-            context.bot.send_video(chat_id=update.effective_chat.id, video=open('./media/Sapo.mp4', 'rb'), supports_streaming=True)
+            context.bot.send_video(chat_id=update.effective_chat.id, video=open('./media/sapo/'+file, 'rb'), supports_streaming=True)
+
+def hya(update,context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text='#HYYYYYAAAAAAA_POSTING')
+    context.bot.send_video(chat_id=update.effective_chat.id, video=open('./media/Hya.mp4', 'rb'), supports_streaming=True)
 
 def main():
     Boturu = telegram.Bot(token = TOKEN)
@@ -490,6 +495,7 @@ def main():
     dp.add_handler(CommandHandler('decepcion', decepcion))
     dp.add_handler(CommandHandler('tragatela', tragatela))
     dp.add_handler(CommandHandler('sapo', sapo))
+    dp.add_handler(CommandHandler('hya', hya))
     run(updater)
     
 if __name__ == '__main__':
